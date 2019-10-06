10195d3adbe200d00a42e25a507b06596b4d0784
dancar528 <dancar.528@gmail.com>
2019-10-06 10:02:06 -0500

    3.10 phonebook backend step10

--- part3/nodejs_and_express/index.js
+++ part3/nodejs_and_express/index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 let persons = [
     {
         id: 1,
