479038e8a27cd6b14900be0818474da52ad65143
dancar528 <dancar.528@gmail.com>
2019-10-13 12:18:50 -0500

    4.4: helper functions and unit tests, step2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let acumulator = 0
+    blogs.forEach(element => {
+        acumulator += element.likes
+    })
+    return acumulator
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
